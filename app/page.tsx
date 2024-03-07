2b5d3cfc5e25aa191c836e973fa251ecef1c6348
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-03-07 22:01:17 +0000

    Feat(app): Added Latest Posts

--- app/page.tsx
+++ app/page.tsx
@@ -1,63 +1,34 @@
-import { Button } from "@/components/ui/button";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel"
 import {
   Card,
-  CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
+} from "@/components/ui/card" 
 
+import { getAllArticles } from "@/lib/articles";
 
 export default function Home() {
+  const articles = getAllArticles()
+
   return (
     <>
       <h1 className="p-10 text-center text-4xl">Steady Ground</h1>
 
-      <Carousel>
-        <CarouselContent>
-          <CarouselItem className="basis-1/3">
-            <Card>
-              <CardHeader>
-                <CardTitle>Card Title</CardTitle>
-                <CardDescription>Card Description</CardDescription>
-              </CardHeader>
-              <CardContent>
-                <p>Card Content</p>
-              </CardContent>
-              <CardFooter>
-                <p>Card Footer</p>
-              </CardFooter>
-            </Card>
-
-          </CarouselItem>
-          <CarouselItem className="basis-1/3">
-            <Card>
-              <CardHeader>
-                <CardTitle>Card Title</CardTitle>
-                <CardDescription>Card Description</CardDescription>
-              </CardHeader>
-              <CardContent>
-                <p>Card Content</p>
-              </CardContent>
-              <CardFooter>
-                <p>Card Footer</p>
-              </CardFooter>
-            </Card>
-          </CarouselItem>
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel >
-
-      <Button>Click me</Button>
-    </>
+      <div className="flex justify-center">
+        <h2 className="text-2xl text-left">Your Articles</h2>
+      </div>
+      {articles.map(article => (
+        <div key={article.name} className="flex justify-center">
+          <Card className="max-w-sm rounded overflow-hidden shadow-lg m-4">
+            <CardHeader>
+              <a href={`/articles/${article.name}`} className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">
+                <CardTitle>{article.title}</CardTitle>
+              </a>
+              <CardDescription className="mt-2 text-gray-500">{article.description}</CardDescription>
+            </CardHeader>
+          </Card>
+        </div>
+))}
+   </>
   );
 }
