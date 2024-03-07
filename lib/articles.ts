fc2d876c5704199a467607231852e103c3cfe3f3
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-03-07 21:14:13 +0000

    Feat(articles): Source Technical File

--- lib/articles.ts
+++ lib/articles.ts
@@ -2,11 +2,10 @@ import fs from "fs"
 import path from "path"
 
 import matter from "gray-matter"
-import { notFound } from "next/navigation";
 
-type GetPostProps = { name: String }
+type ArticleNameProps = { name: String }
 
-export async function getPost({ name }: GetPostProps ) {
+export async function getArticle({ name }: ArticleNameProps) {
   const markdownFile = fs.readFileSync(
     path.join("content/articles", name + ".mdx"),
     "utf-8"
@@ -18,18 +17,19 @@ export async function getPost({ name }: GetPostProps ) {
     name,
     content,
   };
+
 }
 
-export function getAllPosts() {
-  const postsDirectory = path.join(process.cwd(), "content/articles");
+export function getAllArticles() {
+  const articleDirectory = path.join(process.cwd(), "content/articles");
   const fileNames = fs
-    .readdirSync(postsDirectory)
+    .readdirSync(articleDirectory)
     .filter(
       (fileName) => !fileName.startsWith(".") && fileName.endsWith(".mdx")
     );
 
-  const posts = fileNames.map((fileName) => {
-    const fullPath = path.join(postsDirectory, fileName);
+  const articles= fileNames.map((fileName) => {
+    const fullPath = path.join(articleDirectory, fileName);
     const fileContents = fs.readFileSync(fullPath, "utf8");
     const { data: frontMatter } = matter(fileContents);
 
@@ -39,5 +39,39 @@ export function getAllPosts() {
     };
   });
 
-  return posts;
+  return articles;
+}
+
+type ParamsArray = { name: String }[]
+
+export function getStaticParams(files: string[]): ParamsArray {
+  let params = []
+
+  for (const filename of files) {
+    const fullPath = path.join("content/articles", filename);
+    const fileContents = fs.readFileSync(fullPath, "utf8");
+
+    const { data: frontMatter } = matter(fileContents);
+
+    const articleDate = new Date(frontMatter.date);
+    const currentDate = new Date();
+    const isFuture = articleDate > currentDate;
+
+    if (!isFuture) {
+      params.push({ name: filename.replace(".mdx", "") });
+    }
+  }
+
+  return params
+}
+
+export async function getMetadataFields({ name }: ArticleNameProps) {
+  const article = (await getArticle({ name }))
+
+  const title = article.frontMatter.title;
+  const description = article.frontMatter.description;
+
+  const canonicalURL = `${process.env.next_public_base_url}/${name}`;
+
+  return { title, description, canonicalURL }
 }
