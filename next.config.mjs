08e5c6492f1d5cde0037125c73e2139647580288
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-03-07 20:09:10 +0000

    Feat(next.config.mjs): Added MDX Provider to NextJS

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
+import remarkGfm from 'remark-gfm'
+import createMDX from '@next/mdx'
+ 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+}
+ 
+const withMDX = createMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [],
+  },
+})
+ 
+export default withMDX(nextConfig)
