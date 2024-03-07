b7e58b88dc1e3a6b951ac777a1ec2aaf163fab23
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-03-07 21:34:55 +0000

    Feat(TheMDXRemote): Styled Headings

--- components/MDX/TheMDXRemote.tsx
+++ components/MDX/TheMDXRemote.tsx
@@ -4,6 +4,13 @@ type TheMDXRemoteProps = { content: string }
 
 export default function TheMDXRemote({ content }: TheMDXRemoteProps) {
   const components = {
+    h1: (props: {}) => <h1 className="text-2xl font-medium tracking-tighter" {...props} />,
+    h2: (props: {}) => <h1 className="text-2xl font-medium tracking-tighter" {...props} />,
+    h3: (props: {}) => <h1 className="text-2xl font-medium tracking-tighter" {...props} />,
+    h4: (props: {}) => <h1 className="text-2xl font-medium tracking-tighter" {...props} />,
+    h5: (props: {}) => <h1 className="text-2xl  font-medium tracking-tighter" {...props} />,
+
+    ol: (props: {}) => <ol className="list-decimal" {...props} />
   }
 
   return (
