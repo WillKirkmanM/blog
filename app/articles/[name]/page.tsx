2ef2ef57864c688f29d15f1e1b159fa12096fdf1
WillKirkmanM <willkirkmanmoller@gmail.com>
2024-03-25 13:34:01 +0000

    Feat(articles): Add Support for Word Documents

--- app/articles/[name]/page.tsx
+++ app/articles/[name]/page.tsx
@@ -2,12 +2,13 @@ import fs from "fs";
 import path from "path";
 
 import { MDXRemote } from "next-mdx-remote/rsc";
-import { getMetadataFields, getArticle, getStaticParams } from "@/lib/articles"
+import { getArticle } from "@/lib/articles";
+import { getMetadataFields, getStaticParams } from "@/lib/articles"
 import TheMDXRemote from "@/components/MDX/TheMDXRemote";
 
 type Parameters = {
-  params: { 
-    name: string 
+  params: {
+    name: string
   };
 };
 
@@ -30,7 +31,7 @@ export async function generateStaticParams() {
   return params;
 }
 
-export default async function BlogPage({params}: Parameters) {
+export default async function BlogPage({ params }: Parameters) {
   const props = await getArticle(params);
 
   const components = {};
@@ -39,11 +40,23 @@ export default async function BlogPage({params}: Parameters) {
     <>
       <div className="antialiased max-w-2xl mb-40 mx-4 mt-8 lg:mx-auto flex flex-col items-start">
         <div className="title font-medium text-3xl tracking-tighter max-w-[1000px]">
-          <h1>{props.frontMatter.title}</h1>
+          {props.frontMatter && (
+            <h1>{props.frontMatter.title}</h1>
+          )}
+
+          {props.HTMLContent && (
+            <h1>Word Document</h1>
+          )}
         </div>
 
         <div className="prose prose-quoteless prose-neutral dark:prose-invert">
-          <TheMDXRemote content={props.content} />
+          {props.content && (
+            <TheMDXRemote content={props.content} />
+          )}
+
+          {props.HTMLContent && (
+            <div dangerouslySetInnerHTML={{ __html: props.HTMLContent }}></div>
+          )}
         </div>
       </div>
     </>
