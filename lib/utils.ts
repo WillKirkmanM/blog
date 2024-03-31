95c0b1cc9a321ab47abe4db36bebec0e00ac6bf4
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-03-31 17:04:20 +0100

    Feat(utils.ts): Move FormatLastDate function due to client component import issues

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,17 +7,3 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-/**
- * This function returns the last modified date in DD/MM/YY format.
- * @param {string} path
- * @returns {string} 
- */
-export function formattedLastModifiedDate(path: string): string {
-    const lastModified = statSync(path).mtime;
-    const lastModifiedDate = new Date(lastModified);
-    const formattedLastModified = `${("0" + lastModifiedDate.getDate()).slice(-2)}/${("0" + (lastModifiedDate.getMonth() + 1)).slice(-2)}/${lastModifiedDate.getFullYear().toString().substr(-2)}`;
-
-    return formattedLastModified
-}
-
-formattedLastModifiedDate
